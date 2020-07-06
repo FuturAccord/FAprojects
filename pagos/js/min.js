a9dd8e623d956f82d1c366450d970363ad20c43e
FA <47478403+FA@users.noreply.github.com>
2020-07-06 22:57:12 +0100

    v1.0.0.5

--- pagos/js/min.js
+++ pagos/js/min.js
@@ -153,24 +153,22 @@ $(document).on("change", ".sumSemana-2", function() {
 
 
 /* BEGIN current day selected*/ 
-$(document).ready(function(){
     var intervalDay;
-    var days = ['Domingo','Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes','Sabado'];
-    
     
     //every day change color of head Day
     intervalDay = setInterval(function(){
-    var d = new Date();
-    var dayName = days[d.getDay()]; 
-    $("th:contains('" + dayName  + "')").css('backgroundColor','red');
-    for(let i=0; i < 7; i++){
-        if(d.getDay() == i) continue;
-        $("th:contains('" + days[i] + "')").css('backgroundColor',''); 
-    }    
+        var days = ['Domingo','Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes','Sabado'];
+        var d = new Date();
+        var dayName = days[d.getDay()]; 
+        $("th:contains('" + dayName  + "')").css('backgroundColor','red');
+        for(let i=0; i < 7; i++){
+            if(d.getDay() == i) continue;
+            $("th:contains('" + days[i] + "')").css('backgroundColor',''); 
+        }    
     },1000 * 60 * 60 * 4 );
     // console.log(dayName);
-
-}); 
+    
+ 
 /* END current day selected*/
 
 
